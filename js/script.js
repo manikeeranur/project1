ec223f8ac2e60dfdd0260fe6d3ccf84db22c2141
manikeeranur <63514297+manikeeranur@users.noreply.github.com>
2021-04-14 01:19:14 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let icon = document.querySelector("#carouselId");
+icon.addEventListener("click", carouselCaption);
+function carouselCaption() {
+  console.log("clicked");
+//   let h1 = document.querySelector(".moveHeading");
+  //   h1.style.animation = "moveHeading 1s";
+  //   h1.style.transform = "translateX(0px)";
+//   h1.style.cssText = "transform:translateX(0px);animation:moveHeading;";
+
+}
